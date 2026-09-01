02396ec3dccef5858703e67f96b4efde9e8b6494
qaikalll <ednvines@gmail.com>
2026-09-01 11:13:47 +0800

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL="https://vobigffqazuoniptwxed.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY="sb_publishable_zqzwkJ2U1ycTSry2Nwjx7w_QiQnD3Um";
+
+window.supabaseClient=supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
